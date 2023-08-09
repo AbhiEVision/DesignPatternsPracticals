992999915d60d2e9e35d6f267895a2809af876dc
Abhi Patel <abhi.d@simformsolutions.com>
2023-08-09 17:19:19 +0530

    add : log attribute which are registered in singleton manner
    #Singleton

---
+++ DesignPattern.API/Attribute/LogAttribute.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Serilog;
+
+namespace DesignPattern.API.Attribute
+{
+	public class LogAttribute : ActionFilterAttribute
+	{
+		public override void OnActionExecuting(ActionExecutingContext context)
+		{
+			string actionName = ((ControllerActionDescriptor)context.ActionDescriptor).ActionName;
+			var parameters = context.ActionArguments.Select(a => new { val = a.Value.ToString() }).Select(x => x.val).FirstOrDefault();
+			Log.ForContext<LogAttribute>().Information($"{context.Controller.GetType().Name}/{actionName}/{parameters}");
+		}
+	}
+}
