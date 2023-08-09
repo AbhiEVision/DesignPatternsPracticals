ac43ffbe7f6b65a3bfbb7e049fd77421249979ef
Abhi Patel <abhi.d@simformsolutions.com>
2023-08-09 17:14:24 +0530

    add : employee details model
    #Singleton

---
+++ DesignPattern.Singleton.DAL/Models/EmployeeDetails.cs
@@ -0,0 +1,17 @@
+﻿namespace DesignPattern.Singleton.DAL.Models
+{
+	public class EmployeeDetails
+	{
+		public string Name { get; set; }
+
+		public decimal Salary { get; set; }
+
+		public string Department { get; set; }
+
+		public string EmailAddress { get; set; } = null!;
+
+		public DateOnly JoiningDate { get; set; }
+
+		public string Status { get; set; }
+	}
+}
