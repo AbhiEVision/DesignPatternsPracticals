c0b74994f962a1b78e6a485d1df5a6960f49de89
Abhi Patel <abhi.d@simformsolutions.com>
2023-08-09 19:07:06 +0530

    add : department factory and its implementations
    #Factory

---
+++ DesignPattern.Factory.BAL/Factory/DepartmentFactory.cs
@@ -0,0 +1,34 @@
+﻿using DesignPattern.Factory.BAL.Departments;
+using DesignPattern.Factory.BAL.Enum;
+using DesignPattern.Factory.BAL.Interfaces;
+
+namespace DesignPattern.Factory.BAL.Factory
+{
+	public class DepartmentFactory : IDepartmentFactory
+	{
+		public IDepartment GetDepartment(DepartmentEnum department)
+		{
+			switch (department)
+			{
+				case DepartmentEnum.IT:
+					return new ITDepartment();
+
+				case DepartmentEnum.Admin:
+					return new AdminDepartment();
+
+				case DepartmentEnum.HR:
+					return new HRDepartment();
+
+				case DepartmentEnum.Sales:
+					return new SalesDepartment();
+
+				case DepartmentEnum.On_Site:
+					return new OnSiteDepartment();
+
+				default:
+					throw new ArgumentException("No Employee Found");
+
+			}
+		}
+	}
+}
---
+++ DesignPattern.Factory.BAL/Interfaces/IDepartmentFactory.cs
@@ -0,0 +1,9 @@
+﻿using DesignPattern.Factory.BAL.Enum;
+
+namespace DesignPattern.Factory.BAL.Interfaces
+{
+	public interface IDepartmentFactory
+	{
+		IDepartment GetDepartment(DepartmentEnum department);
+	}
+}
