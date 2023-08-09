579c465885596d25eef3f2c7047b02d88baf2855
Abhi Patel <abhi.d@simformsolutions.com>
2023-08-09 19:09:47 +0530

    add : employee calculations

---
+++ DesignPattern.Factory.BAL/EmployeeCalculations.cs
@@ -0,0 +1,29 @@
+﻿using DesignPattern.Factory.BAL.Database;
+using DesignPattern.Factory.BAL.Enum;
+using DesignPattern.Factory.BAL.Interfaces;
+
+namespace DesignPattern.Factory.BAL
+{
+	public class EmployeeCalculations
+	{
+		private readonly IDepartmentFactory _departmentFactory;
+		private readonly ManageDatabaseForFactory _connectionClass;
+
+		public EmployeeCalculations(IDepartmentFactory departmentFactory, ManageDatabaseForFactory connectionClass)
+		{
+			_departmentFactory = departmentFactory;
+			_connectionClass = connectionClass;
+		}
+
+		public async Task<double> DoOvertimeCalculation(int empId, int hours)
+		{
+			int depId = await _connectionClass.GetEmployeeDepartment(empId);
+
+			IDepartment department = _departmentFactory.GetDepartment((DepartmentEnum)depId);
+
+			double hourlyOverPay = department.CalculateOverTimePay(hours);
+
+			return hourlyOverPay;
+		}
+	}
+}
