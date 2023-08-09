3ac6caafbc2dd9c657e61bc54f9381e445cf42f7
AbhiDashadiya <pabhi1504@gmail.com>
2023-08-09 22:33:44 +0530

    add : abstract factory and its implementations

---
+++ DesignPatterns.AbstractFactory.BAL/Implemantations/Factories/AbstractFactory.cs
@@ -0,0 +1,22 @@
+﻿using DesignPatterns.AbstractFactory.BAL.Interfaces;
+
+namespace DesignPatterns.AbstractFactory.BAL.Implemantations.Factories
+{
+	public class AbstractFactory : IAbstractFactory
+	{
+		public IFactory GetFactory(string typeOfFactory)
+		{
+			switch (typeOfFactory)
+			{
+				case "Indoor":
+					return new IndoorFactory();
+
+				case "Outdoor":
+					return new OutdoorFactory();
+
+				default:
+					throw new ArgumentException("Plese enter valid Factory Type");
+			}
+		}
+	}
+}
---
+++ DesignPatterns.AbstractFactory.BAL/Interfaces/IAbstractFactory.cs
@@ -0,0 +1,7 @@
+﻿namespace DesignPatterns.AbstractFactory.BAL.Interfaces
+{
+	public interface IAbstractFactory
+	{
+		IFactory GetFactory(string typeOfFactory);
+	}
+}
