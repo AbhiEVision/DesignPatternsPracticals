f8e1f3caf602cb88a932e44918f82549178a7d7d
Abhi Patel <abhi.d@simformsolutions.com>
2023-08-09 17:14:54 +0530

    add : model for creating or update employee
    #Singleton

---
+++ DesignPattern.Singleton.DAL/Models/CreateOrUpdateEmployeeDetails.cs
@@ -0,0 +1,14 @@
+﻿namespace DesignPattern.Singleton.DAL.Models
+{
+	public class CreateOrUpdateEmployeeDetails
+	{
+		public string Name { get; set; } = null!;
+
+		public decimal Salary { get; set; }
+
+		public int DepartmentId { get; set; }
+
+		public string EmailAddress { get; set; } = null!;
+
+	}
+}
