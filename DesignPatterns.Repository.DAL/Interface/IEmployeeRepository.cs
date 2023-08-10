68ae0d9c9689367f5718fce49a307febe95d7ad9
Abhi Patel <abhi.d@simformsolutions.com>
2023-08-10 12:50:38 +0530

    add : repository interface and its implementations

---
+++ DesignPatterns.Repository.DAL/Interface/IEmployeeRepository.cs
@@ -0,0 +1,17 @@
+﻿using DesignPatterns.Repository.DAL.Models;
+
+namespace DesignPatterns.Repository.DAL.Interface
+{
+	public interface IEmployeeRepository
+	{
+		Task<bool> CreateEmployeeAsync(CreateOrUpdateEmployeeDetailsRepo employeeDetails);
+
+		Task<bool> UpdateEmployeeAsync(int empId, CreateOrUpdateEmployeeDetailsRepo employeeDetails);
+
+		Task<bool> DeleteEmployeeAsync(int empId);
+
+		Task<IEnumerable<EmployeeDetailsRepo>> GetEmployeeDetailsAsync();
+
+		Task<EmployeeDetailsRepo> GetEmployeeDetailAsync(int empId);
+	}
+}
---
+++ DesignPatterns.Repository.DAL/Repositories/EmployeeRepository.cs
@@ -0,0 +1,43 @@
+﻿using DesignPatterns.Repository.DAL.Database;
+using DesignPatterns.Repository.DAL.Interface;
+using DesignPatterns.Repository.DAL.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace DesignPatterns.Repository.DAL.Repositories
+{
+	public class EmployeeRepository : IEmployeeRepository
+	{
+		private readonly ManageDatabaseForRepository _databse;
+
+		public EmployeeRepository(IConfiguration configuration)
+		{
+			_databse = new ManageDatabaseForRepository(configuration.GetConnectionString("Default"));
+		}
+
+		public async Task<bool> CreateEmployeeAsync(CreateOrUpdateEmployeeDetailsRepo employeeDetails)
+		{
+			return await _databse.CreateEmployeeAsync(employeeDetails);
+		}
+
+		public async Task<bool> UpdateEmployeeAsync(int empId, CreateOrUpdateEmployeeDetailsRepo employeeDetails)
+		{
+			return await _databse.UpdateEmployeeAsync(empId, employeeDetails);
+		}
+
+		public async Task<bool> DeleteEmployeeAsync(int empId)
+		{
+			return await _databse.DeleteEmployeeAsync(empId);
+		}
+
+		public async Task<IEnumerable<EmployeeDetailsRepo>> GetEmployeeDetailsAsync()
+		{
+			return await _databse.GetAllEmployeesAsync();
+		}
+
+		public async Task<EmployeeDetailsRepo> GetEmployeeDetailAsync(int empId)
+		{
+			return await _databse.GetEmployeeDetailsAsync(empId);
+		}
+
+	}
+}
