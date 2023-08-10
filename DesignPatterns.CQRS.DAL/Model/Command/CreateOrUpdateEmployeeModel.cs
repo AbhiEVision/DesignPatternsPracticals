53714a1b0a117339b8fa085f6e6865df2b851a99
Abhi Patel <abhi.d@simformsolutions.com>
2023-08-10 16:10:23 +0530

    add : model for command and query repository

---
+++ DesignPatterns.CQRS.DAL/Model/Command/CreateOrUpdateEmployeeModel.cs
@@ -0,0 +1,14 @@
+﻿namespace DesignPatterns.CQRS.DAL.Model.Command
+{
+	public class CreateOrUpdateEmployeeModel
+	{
+		public string Name { get; set; } = null!;
+
+		public decimal Salary { get; set; }
+
+		public int DepartmentId { get; set; }
+
+		public string EmailAddress { get; set; } = null!;
+
+	}
+}
---
+++ DesignPatterns.CQRS.DAL/Model/Query/EmployeeDetails.cs
@@ -0,0 +1,17 @@
+﻿namespace DesignPatterns.CQRS.DAL.Model.Query
+{
+	public class EmployeeDetails
+	{
+		public string Name { get; set; }
+
+		public decimal Salary { get; set; }
+
+		public string Department { get; set; }
+
+		public string EmailAddress { get; set; } = null!;
+
+		public DateOnly JoiningDate { get; set; }
+
+		public string Status { get; set; }
+	}
+}
