2cc8bef912bbc6e23d403950904c6d7a32b7d062
Abhi Patel <abhi.d@simformsolutions.com>
2023-08-10 16:08:56 +0530

    add : command repository and its implementations

---
+++ DesignPatterns.CQRS.DAL/Implementation/Repository/CommandRepository.cs
@@ -0,0 +1,32 @@
+﻿using DesignPatterns.CQRS.DAL.Database;
+using DesignPatterns.CQRS.DAL.Interface;
+using DesignPatterns.CQRS.DAL.Model.Command;
+using Microsoft.Extensions.Configuration;
+
+namespace DesignPatterns.CQRS.DAL.Implementation.Repository
+{
+	public class CommandRepository : ICommandRepository
+	{
+		public readonly ManageDatabaseForCQRS _database;
+
+		public CommandRepository(IConfiguration configuration)
+		{
+			_database = new ManageDatabaseForCQRS(configuration.GetConnectionString("Default"));
+		}
+
+		public async Task<bool> CreateEmployee(CreateOrUpdateEmployeeModel employee)
+		{
+			return await _database.CreateEmployeeAsync(employee);
+		}
+
+		public async Task<bool> DeleteEmployee(int empId)
+		{
+			return await _database.DeleteEmployeeAsync(empId);
+		}
+
+		public async Task<bool> UpdateEmployee(int empId, CreateOrUpdateEmployeeModel employee)
+		{
+			return await _database.UpdateEmployeeAsync(empId, employee);
+		}
+	}
+}
---
+++ DesignPatterns.CQRS.DAL/Interface/ICommandRepository.cs
@@ -0,0 +1,13 @@
+﻿using DesignPatterns.CQRS.DAL.Model.Command;
+
+namespace DesignPatterns.CQRS.DAL.Interface
+{
+	public interface ICommandRepository
+	{
+		Task<bool> CreateEmployee(CreateOrUpdateEmployeeModel employee);
+
+		Task<bool> UpdateEmployee(int empId, CreateOrUpdateEmployeeModel employee);
+
+		Task<bool> DeleteEmployee(int empId);
+	}
+}
