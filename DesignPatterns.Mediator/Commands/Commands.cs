b818bd1b632c5a13c9a5c78fcf2dec82c7b750e6
Abhi Patel <abhi.d@simformsolutions.com>
2023-08-10 18:50:14 +0530

    add : request handler

---
+++ DesignPatterns.Mediator/Commands/Commands.cs
@@ -0,0 +1,57 @@
+﻿using MediatR;
+
+namespace DesignPatterns.Mediator.Commands
+{
+	public class UpdateEmployeeDataCommand : IRequest<bool>
+	{
+		public UpdateEmployeeDataCommand(int id, string name, decimal salary, int departmentId, string emailAddress)
+		{
+			Id = id;
+			Name = name;
+			Salary = salary;
+			DepartmentId = departmentId;
+			EmailAddress = emailAddress;
+		}
+
+		public int Id { get; set; }
+
+		public string Name { get; set; } = null!;
+
+		public decimal Salary { get; set; }
+
+		public int DepartmentId { get; set; }
+
+		public string EmailAddress { get; set; } = null!;
+
+	}
+
+	public class CreateEmployeeCommand : IRequest<bool>
+	{
+		public CreateEmployeeCommand(string name, decimal salary, int departmentId, string emailAddress)
+		{
+			Name = name;
+			Salary = salary;
+			DepartmentId = departmentId;
+			EmailAddress = emailAddress;
+		}
+
+		public string Name { get; set; } = null!;
+
+		public decimal Salary { get; set; }
+
+		public int DepartmentId { get; set; }
+
+		public string EmailAddress { get; set; } = null!;
+
+	}
+
+	public class DeleteEmployeeCommand : IRequest<bool>
+	{
+		public DeleteEmployeeCommand(int id)
+		{
+			Id = id;
+		}
+
+		public int Id { get; set; }
+	}
+}
---
+++ DesignPatterns.Mediator/Queries/Queries.cs
@@ -0,0 +1,15 @@
+﻿using DesignPatterns.Repository.DAL.Models;
+using MediatR;
+
+namespace DesignPatterns.Mediator.Queries
+{
+	public class GetEmployeeByIdQuery : IRequest<EmployeeDetailsRepo>
+	{
+		public int Id { get; set; }
+	}
+
+	public class GetEmployeeDetailsQuery : IRequest<List<EmployeeDetailsRepo>>
+	{
+
+	}
+}
