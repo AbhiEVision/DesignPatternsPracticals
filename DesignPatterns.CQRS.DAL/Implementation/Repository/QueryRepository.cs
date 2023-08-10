8045ef623e0ff5fb14d4fe4ce6d74f4d1f63840c
Abhi Patel <abhi.d@simformsolutions.com>
2023-08-10 16:09:24 +0530

    add : query repository and its implementations

---
+++ DesignPatterns.CQRS.DAL/Implementation/Repository/QueryRepository.cs
@@ -0,0 +1,26 @@
+﻿using DesignPatterns.CQRS.DAL.Database;
+using DesignPatterns.CQRS.DAL.Interface;
+using DesignPatterns.CQRS.DAL.Model.Query;
+using Microsoft.Extensions.Configuration;
+
+namespace DesignPatterns.CQRS.DAL.Implementation.Repository
+{
+	public class QueryRepository : IQueryRepository
+	{
+		public readonly ManageDatabaseForCQRS _database;
+
+		public QueryRepository(IConfiguration configuration)
+		{
+			_database = new ManageDatabaseForCQRS(configuration.GetConnectionString("Default"));
+		}
+		public async Task<IEnumerable<EmployeeDetails>> GetEmployeeDetails()
+		{
+			return await _database.GetAllEmployeesAsync();
+		}
+
+		public async Task<EmployeeDetails> GetEmployeeDetails(int id)
+		{
+			return await _database.GetEmployeeDetailsAsync(id);
+		}
+	}
+}
---
+++ DesignPatterns.CQRS.DAL/Interface/IQueryRepository.cs
@@ -0,0 +1,11 @@
+﻿using DesignPatterns.CQRS.DAL.Model.Query;
+
+namespace DesignPatterns.CQRS.DAL.Interface
+{
+	public interface IQueryRepository
+	{
+		Task<IEnumerable<EmployeeDetails>> GetEmployeeDetails();
+
+		Task<EmployeeDetails> GetEmployeeDetails(int id);
+	}
+}
