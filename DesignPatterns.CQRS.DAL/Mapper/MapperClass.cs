911bb5a46da0a9397d9f5069354e29f94627e362
Abhi Patel <abhi.d@simformsolutions.com>
2023-08-10 16:09:43 +0530

    add : mapper class for mapping data

---
+++ DesignPatterns.CQRS.DAL/Mapper/MapperClass.cs
@@ -0,0 +1,44 @@
+﻿using DesignPatterns.CQRS.DAL.Model.Query;
+using System.Data.SqlClient;
+
+namespace DesignPatterns.CQRS.DAL.Mapper
+{
+	public class MapperClass
+	{
+		public static List<EmployeeDetails> MapEmployeeListFromSqlDataReader(SqlDataReader reader)
+		{
+			List<EmployeeDetails> employees = new List<EmployeeDetails>();
+
+			while (reader.Read())
+			{
+				EmployeeDetails employee = new EmployeeDetails();
+				employee.Name = reader.GetString(0);
+				employee.Salary = reader.GetDecimal(1);
+				employee.Department = reader.GetString(2);
+				employee.EmailAddress = reader.GetString(3);
+				var datedTime = reader.GetDateTime(4);
+				employee.JoiningDate = new DateOnly(datedTime.Year, datedTime.Month, datedTime.Day);
+				employee.Status = reader.GetString(5);
+				employees.Add(employee);
+			}
+			return employees;
+		}
+
+		public static EmployeeDetails MapEmployeeFromSqlDataReader(SqlDataReader reader)
+		{
+			var employee = new EmployeeDetails();
+			if (!reader.Read())
+			{
+				return null;
+			}
+			employee.Name = reader.GetString(0);
+			employee.Salary = reader.GetDecimal(1);
+			employee.Department = reader.GetString(2);
+			employee.EmailAddress = reader.GetString(3);
+			var datedTime = reader.GetDateTime(4);
+			employee.JoiningDate = new DateOnly(datedTime.Year, datedTime.Month, datedTime.Day);
+			employee.Status = reader.GetString(5);
+			return employee;
+		}
+	}
+}
