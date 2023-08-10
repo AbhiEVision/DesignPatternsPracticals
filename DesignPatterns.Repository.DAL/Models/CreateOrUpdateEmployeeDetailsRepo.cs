23725ab6b3d4f033d1f611f4da0d5c713ab14570
Abhi Patel <abhi.d@simformsolutions.com>
2023-08-10 12:48:59 +0530

    add : model for repository pattern

---
+++ DesignPatterns.Repository.DAL/Models/CreateOrUpdateEmployeeDetailsRepo.cs
@@ -0,0 +1,14 @@
+﻿namespace DesignPatterns.Repository.DAL.Models
+{
+	public class CreateOrUpdateEmployeeDetailsRepo
+	{
+		public string Name { get; set; } = null!;
+
+		public decimal Salary { get; set; }
+
+		public int DepartmentId { get; set; }
+
+		public string EmailAddress { get; set; } = null!;
+
+	}
+}
---
+++ DesignPatterns.Repository.DAL/Models/EmployeeDetailsRepo.cs
@@ -0,0 +1,17 @@
+﻿namespace DesignPatterns.Repository.DAL.Models
+{
+	public class EmployeeDetailsRepo
+	{
+		public string Name { get; set; }
+
+		public decimal Salary { get; set; }
+
+		public string Department { get; set; }
+
+		public string EmailAddress { get; set; } = null!;
+
+		public DateOnly JoiningDate { get; set; }
+
+		public string Status { get; set; }
+	}
+}
