171b6bd477bf1d56ccb43ced1008e7a1b4b9f132
Abhi Patel <abhi.d@simformsolutions.com>
2023-08-10 18:50:40 +0530

    add : request handler which will handle the requests

---
+++ DesignPatterns.Mediator/Handlers/CommandHandlers.cs
@@ -0,0 +1,63 @@
+﻿using DesignPatterns.Mediator.Commands;
+using DesignPatterns.Repository.DAL.Interface;
+using DesignPatterns.Repository.DAL.Models;
+using MediatR;
+
+namespace DesignPatterns.Mediator.Handlers
+{
+	public class UpdateEmployeeDataCommandHandler : IRequestHandler<UpdateEmployeeDataCommand, bool>
+	{
+		private readonly IEmployeeRepository _repository;
+
+		public UpdateEmployeeDataCommandHandler(IEmployeeRepository repository)
+		{
+			_repository = repository;
+		}
+
+		async Task<bool> IRequestHandler<UpdateEmployeeDataCommand, bool>.Handle(UpdateEmployeeDataCommand request, CancellationToken cancellationToken)
+		{
+			CreateOrUpdateEmployeeDetailsRepo model = new CreateOrUpdateEmployeeDetailsRepo();
+			model.EmailAddress = request.EmailAddress;
+			model.Salary = request.Salary;
+			model.DepartmentId = request.DepartmentId;
+			model.Name = request.Name;
+
+			return await _repository.UpdateEmployeeAsync(request.Id, model);
+		}
+	}
+	public class CreateEmployeeCommandHandler : IRequestHandler<CreateEmployeeCommand, bool>
+	{
+		private readonly IEmployeeRepository _repository;
+
+		public CreateEmployeeCommandHandler(IEmployeeRepository repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task<bool> Handle(CreateEmployeeCommand request, CancellationToken cancellationToken)
+		{
+			CreateOrUpdateEmployeeDetailsRepo model = new CreateOrUpdateEmployeeDetailsRepo();
+			model.EmailAddress = request.EmailAddress;
+			model.Salary = request.Salary;
+			model.DepartmentId = request.DepartmentId;
+			model.Name = request.Name;
+
+			return await _repository.CreateEmployeeAsync(model);
+		}
+	}
+
+	public class DeleteEmployeeCommandHandler : IRequestHandler<DeleteEmployeeCommand, bool>
+	{
+		private readonly IEmployeeRepository _repository;
+
+		public DeleteEmployeeCommandHandler(IEmployeeRepository repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task<bool> Handle(DeleteEmployeeCommand request, CancellationToken cancellationToken)
+		{
+			return await _repository.DeleteEmployeeAsync(request.Id);
+		}
+	}
+}
---
+++ DesignPatterns.Mediator/Handlers/QueryHandlers.cs
@@ -0,0 +1,37 @@
+﻿using DesignPatterns.Mediator.Queries;
+using DesignPatterns.Repository.DAL.Interface;
+using DesignPatterns.Repository.DAL.Models;
+using MediatR;
+
+namespace DesignPatterns.Mediator.Handlers
+{
+	public class GetEmployeeByIdQueryHandler : IRequestHandler<GetEmployeeByIdQuery, EmployeeDetailsRepo>
+	{
+		private readonly IEmployeeRepository _repository;
+
+		public GetEmployeeByIdQueryHandler(IEmployeeRepository repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task<EmployeeDetailsRepo> Handle(GetEmployeeByIdQuery request, CancellationToken cancellationToken)
+		{
+			return await _repository.GetEmployeeDetailAsync(request.Id);
+		}
+	}
+
+	public class GetEmployeeDetailsQueryHandler : IRequestHandler<GetEmployeeDetailsQuery, List<EmployeeDetailsRepo>>
+	{
+		private readonly IEmployeeRepository _repository;
+
+		public GetEmployeeDetailsQueryHandler(IEmployeeRepository repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task<List<EmployeeDetailsRepo>> Handle(GetEmployeeDetailsQuery request, CancellationToken cancellationToken)
+		{
+			return (await _repository.GetEmployeeDetailsAsync()).ToList();
+		}
+	}
+}
