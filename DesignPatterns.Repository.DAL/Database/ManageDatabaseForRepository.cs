6b18660e6133264a08f534e998fa5f412d01884d
Abhi Patel <abhi.d@simformsolutions.com>
2023-08-10 12:49:33 +0530

    add : database connection and communication class

---
+++ DesignPatterns.Repository.DAL/Database/ManageDatabaseForRepository.cs
@@ -0,0 +1,137 @@
+﻿using DesignPatterns.Repository.DAL.MapperClasses;
+using DesignPatterns.Repository.DAL.Models;
+using System.Data.SqlClient;
+
+namespace DesignPatterns.Repository.DAL.Database
+{
+	public class ManageDatabaseForRepository
+	{
+		private readonly string _connectionString;
+
+		public ManageDatabaseForRepository(string connectionString)
+		{
+			_connectionString = connectionString;
+		}
+
+		public async Task<List<EmployeeDetailsRepo>> GetAllEmployeesAsync()
+		{
+
+			using (SqlConnection connection = new SqlConnection(_connectionString))
+			{
+				connection.Open();
+				SqlCommand selectCommand = new SqlCommand(QueriesClass.SelectEmployeeDetails, connection);
+
+				SqlDataReader reader = await selectCommand.ExecuteReaderAsync();
+
+				List<EmployeeDetailsRepo> employees = Mapper.MapEmployeeListFromSqlDataReader(reader);
+
+
+				connection.Close();
+				connection.Dispose();
+				return employees;
+			}
+		}
+
+		public async Task<EmployeeDetailsRepo> GetEmployeeDetailsAsync(int id)
+		{
+			using (SqlConnection connection = new SqlConnection(_connectionString))
+			{
+				connection.Open();
+				SqlCommand selectCommand = new SqlCommand(QueriesClass.SelectEmployeeDetail, connection);
+				selectCommand.Parameters.AddWithValue("id", id);
+
+				SqlDataReader reader = await selectCommand.ExecuteReaderAsync();
+
+				EmployeeDetailsRepo employee = Mapper.MapEmployeeFromSqlDataReader(reader);
+
+				connection.Close();
+				connection.Dispose();
+				return employee;
+			}
+		}
+
+		public async Task<bool> CreateEmployeeAsync(CreateOrUpdateEmployeeDetailsRepo employeeDetails)
+		{
+			using (SqlConnection connection = new SqlConnection(_connectionString))
+			{
+				connection.Open();
+				SqlCommand createCommand = new SqlCommand(QueriesClass.CreateEmployee, connection);
+
+				// Parameters
+				createCommand.Parameters.AddWithValue("name", employeeDetails.Name);
+				createCommand.Parameters.AddWithValue("email", employeeDetails.EmailAddress);
+				createCommand.Parameters.AddWithValue("salary", employeeDetails.Salary);
+				createCommand.Parameters.AddWithValue("departmentId", employeeDetails.DepartmentId);
+
+				int rowAffected = await createCommand.ExecuteNonQueryAsync();
+
+				// Connection Close
+				connection.Close();
+				connection.Dispose();
+
+				if (rowAffected == 1)
+				{
+					return true;
+				}
+
+				return false;
+			}
+		}
+
+
+		public async Task<bool> UpdateEmployeeAsync(int empId, CreateOrUpdateEmployeeDetailsRepo employeeDetails)
+		{
+			using (SqlConnection connection = new SqlConnection(_connectionString))
+			{
+				connection.Open();
+				SqlCommand updateCommand = new SqlCommand(QueriesClass.UpdateEmployee, connection);
+
+				// Parameters
+				updateCommand.Parameters.AddWithValue("name", employeeDetails.Name);
+				updateCommand.Parameters.AddWithValue("email", employeeDetails.EmailAddress);
+				updateCommand.Parameters.AddWithValue("salary", employeeDetails.Salary);
+				updateCommand.Parameters.AddWithValue("depId", employeeDetails.DepartmentId);
+				updateCommand.Parameters.AddWithValue("empId", empId);
+
+				int rowAffected = await updateCommand.ExecuteNonQueryAsync();
+
+				// Connection Close
+				connection.Close();
+				connection.Dispose();
+
+				if (rowAffected == 1)
+				{
+					return true;
+				}
+
+				return false;
+			}
+		}
+
+		public async Task<bool> DeleteEmployeeAsync(int empId)
+		{
+			using (SqlConnection connection = new SqlConnection(_connectionString))
+			{
+				connection.Open();
+				SqlCommand deleteCommand = new SqlCommand(QueriesClass.DeleteEmployee, connection);
+
+				deleteCommand.Parameters.AddWithValue("empId", empId);
+
+				int rowAffected = await deleteCommand.ExecuteNonQueryAsync();
+
+				// Connection Close
+				connection.Close();
+				connection.Dispose();
+
+				if (rowAffected == 1)
+				{
+					return true;
+				}
+
+				return false;
+			}
+		}
+
+	}
+
+}
---
+++ DesignPatterns.Repository.DAL/Database/QueriesClass.cs
@@ -0,0 +1,26 @@
+﻿namespace DesignPatterns.Repository.DAL.Database
+{
+	internal static class QueriesClass
+	{
+		public static readonly string SelectEmployeeDetails = "SELECT emp.Name, emp.Salary, dep.DepartmentName, emp.EmailAddress, emp.JoiningDate, sta.StatusName " +
+			"FROM tblEmployee emp " +
+			"INNER JOIN tblDepartement dep ON dep.DepartmentId = emp.DepartmentId " +
+			"INNER JOIN tblStatus sta ON sta.StatusId = emp.StatusId";
+
+		public static readonly string SelectEmployeeDetail = "SELECT emp.Name, emp.Salary, dep.DepartmentName, emp.EmailAddress, emp.JoiningDate, sta.StatusName " +
+			"FROM tblEmployee emp " +
+			"INNER JOIN tblDepartement dep ON dep.DepartmentId = emp.DepartmentId " +
+			"INNER JOIN tblStatus sta ON sta.StatusId = emp.StatusId " +
+			"where emp.EmployeeId = @id";
+
+		public static readonly string CreateEmployee = "INSERT INTO tblEmployee(Name,Salary,DepartmentId,EmailAddress) " +
+			"VALUES ( @name, @salary, @departmentId, @email)";
+
+		public static readonly string UpdateEmployee = "UPDATE tblEmployee " +
+			"SET Name = @name, Salary = @salary, DepartmentId = @depId, EmailAddress = @email WHERE EmployeeId = @empId";
+
+		public static readonly string DeleteEmployee = "UPDATE tblEmployee " +
+			"SET isActive = 0 WHERE EmployeeId = @empId";
+
+	}
+}
