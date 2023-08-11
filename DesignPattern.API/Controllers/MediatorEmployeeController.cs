7362e1a22b6a76a24c593eb56bf26ebfc7d0d8a1
Abhi Patel <abhi.d@simformsolutions.com>
2023-08-11 13:31:22 +0530

    update : logic for update and create in mediator pattern

--- DesignPattern.API/Controllers/MediatorEmployeeController.cs
+++ DesignPattern.API/Controllers/MediatorEmployeeController.cs
@@ -1,8 +1,10 @@
-﻿using DesignPatterns.Mediator.Commands;
+﻿using DesignPattern.API.Messages;
+using DesignPatterns.Mediator.Commands;
 using DesignPatterns.Mediator.Queries;
 using DesignPatterns.Repository.DAL.Models;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
 
 namespace DesignPattern.API.Controllers
 {
@@ -25,14 +27,14 @@ namespace DesignPattern.API.Controllers
 				List<EmployeeDetailsRepo> employees = await _mediator.Send(new GetEmployeeDetailsQuery());
 				if (employees == null || employees.Count() == 0)
 				{
-					return Ok("No Employee are Registered");
+					return Ok(ResponseMessage.NoEmployeeDataFound);
 				}
 				return Ok(employees);
 			}
 			EmployeeDetailsRepo employee = await _mediator.Send(new GetEmployeeByIdQuery() { Id = id ?? 1 });
 			if (employee == null)
 			{
-				return Ok("requested Data is not found");
+				return Ok(ResponseMessage.EmployeeIsNotFound);
 			}
 			return Ok(employee);
 		}
@@ -43,17 +45,42 @@ namespace DesignPattern.API.Controllers
 		{
 			if (employeeDetails == null)
 			{
-				return BadRequest("Please enter data");
+				return BadRequest(ResponseMessage.InvalidData);
 			}
 
-			bool created = await _mediator.Send(new CreateEmployeeCommand(employeeDetails.Name, employeeDetails.Salary, employeeDetails.DepartmentId, employeeDetails.EmailAddress));
+			bool created;
+			try
+			{
+				created = await _mediator.Send(new CreateEmployeeCommand(employeeDetails.Name, employeeDetails.Salary, employeeDetails.DepartmentId, employeeDetails.EmailAddress));
+
+			}
+			catch (SqlException ex)
+			{
+				if (ex.Message == "Arithmetic overflow error converting numeric to data type money.\r\nThe statement has been terminated.")
+				{
+					return BadRequest(ResponseMessage.SalaryLimitExceed);
+				}
+				if (ex.Message.Contains("Violation of UNIQUE KEY constraint 'UQ__tblEmplo__49A1474005B85882'"))
+				{
+					return BadRequest(ResponseMessage.EmployeeAlreadyExists);
+				}
+				if (ex.Message.Contains("The INSERT statement conflicted with the FOREIGN KEY constraint \"FK__tblEmploy__Depar__4F7CD00D\""))
+				{
+					return BadRequest(ResponseMessage.DepartmentIsNotFound);
+				}
+				return BadRequest(ex.Message);
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
 
 			if (created)
 			{
-				return Ok("User Created!");
+				return Ok(ResponseMessage.EmployeeCreated);
 			}
 
-			return BadRequest("User is already exists!");
+			return BadRequest(ResponseMessage.EmployeeIsNotCreated);
 		}
 
 		[HttpPut]
@@ -61,17 +88,42 @@ namespace DesignPattern.API.Controllers
 		{
 			if (id == null || id <= 0)
 			{
-				return BadRequest("Please enter valid data!");
+				return BadRequest(ResponseMessage.InvalidData);
 			}
 
-			bool updated = await _mediator.Send(new UpdateEmployeeDataCommand(id, employeeDetails.Name, employeeDetails.Salary, employeeDetails.DepartmentId, employeeDetails.EmailAddress));
+			bool updated;
+
+			try
+			{
+				updated = await _mediator.Send(new UpdateEmployeeDataCommand(id, employeeDetails.Name, employeeDetails.Salary, employeeDetails.DepartmentId, employeeDetails.EmailAddress));
+			}
+			catch (SqlException ex)
+			{
+				if (ex.Message == "Arithmetic overflow error converting numeric to data type money.\r\nThe statement has been terminated.")
+				{
+					return BadRequest(ResponseMessage.SalaryLimitExceed);
+				}
+				if (ex.Message.Contains("Violation of UNIQUE KEY constraint 'UQ__tblEmplo__49A1474005B85882'"))
+				{
+					return BadRequest(ResponseMessage.EmployeeAlreadyExists);
+				}
+				if (ex.Message.Contains("The UPDATE statement conflicted with the FOREIGN KEY constraint \"FK__tblEmploy__Depar__4F7CD00D\"."))
+				{
+					return BadRequest(ResponseMessage.DepartmentIsNotFound);
+				}
+				return BadRequest(ex.Message);
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
 
 			if (updated)
 			{
-				return Ok("Updated successfully!");
+				return Ok(ResponseMessage.EmployeeUpdated);
 			}
 
-			return BadRequest("User is not found!");
+			return BadRequest(ResponseMessage.EmployeeIsNotUpdated);
 
 		}
 
@@ -80,17 +132,17 @@ namespace DesignPattern.API.Controllers
 		{
 			if (empId == null || empId <= 0)
 			{
-				return BadRequest("Please enter the valid data!");
+				return BadRequest(ResponseMessage.InvalidData);
 			}
 
 			bool deleted = await _mediator.Send(new DeleteEmployeeCommand(empId));
 
 			if (deleted)
 			{
-				return Ok("Employee deleted successfully");
+				return Ok(ResponseMessage.EmployeeIsDeleted);
 			}
 
-			return BadRequest("Employee is not found!");
+			return BadRequest(ResponseMessage.EmployeeIsNotFound);
 		}
 	}
 }
